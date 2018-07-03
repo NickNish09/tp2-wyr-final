25510e33b4aa82679bc583d25f3d47a2ecb2f5cd
Nicholas <nicholasmarques@cjr.org.br>
2018-07-03 16:18:08 -0300

    primeiro commit

---
+++ features/support/env.rb
@@ -0,0 +1,17 @@
+require "appium_lib"
+
+def caps
+  {
+      caps: {
+          deviceName: "Nexus_5X_API_27",
+          platformName: "Android",
+          app: (File.join(File.dirname(__FILE__),"wyr.apk")),
+          appPackage: "com.ba.universalconverter",
+          appActivity: "MainConverterActivity",
+          newCommandTimeout: "3600"
+      }
+  }
+end
+
+Appium::Driver.new(caps,true)
+Appium.promote_appium_methods Object
