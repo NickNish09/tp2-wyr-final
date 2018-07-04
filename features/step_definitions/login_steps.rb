d52624620f2bb28701fce199ab60c37894b1cc30
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-07-03 22:01:03 -0300

    done quando bbd for login_steps

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -8,11 +8,13 @@ end
 
 Quando("eu preencher o campo de email com {string} e senha com {string}") do |email, password|
   find_element(id: "emailText").send_keys(email)
+  driver.navigate.back
   find_element(id: "passwordText").send_keys(password)
+  driver.navigate.back
 end
 
 Quando("clicar no botão {string}") do |btn_entrar|
-  find_element(id: btn_entrar).click
+  find_element(id: "signInBtn").click
 end
 
 Então("eu devo ver a mensagem {string}") do |message|
