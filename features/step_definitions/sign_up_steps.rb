f281c3775125ac4835daed6932529a2559542982
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-07-04 01:45:18 -0300

    casos de falha para sign up

--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -3,7 +3,7 @@ Dado("eu clicar no botão de voltar") do
   driver.navigate.back
 end
 
-Quando("eu preencher o campo de email com um novo email e senha {string} e senha com {string}") do |email_novo, senha_nova|
+Quando("eu preencher o campo de email com um novo email {string} e senha {string}") do |email_novo, senha_nova|
   find_element(id: "emailText").send_keys("boni#{Time.now.strftime("%d%m%y%H%M%S")}@facil.com")
   driver.navigate.back
   find_element(id: "passwordText").send_keys(senha_nova)
@@ -13,4 +13,11 @@ end
 
 Quando("eu preencher o campo de confirmar senha com {string}") do |password_confimation_text|
   find_element(id: "passwordTextConfirm").send_keys(password_confimation_text)
+end
+
+Quando("eu preencher o campo de email com um email existente {string} e senha {string}") do |email, senha_nova|
+  find_element(id: "emailText").send_keys(email)
+  driver.navigate.back
+  find_element(id: "passwordText").send_keys(senha_nova)
+  driver.navigate.back
 end
